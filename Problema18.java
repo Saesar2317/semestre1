1d8a2b6538aee77ec6cc7f419e9f2c0ba0dd4adb
Saesar2317 <saesar.vazqueza@gmail.com>
2024-11-07 00:52:46 -0700

    Update Problema18.java

--- Problema18.java
+++ Problema18.java
@@ -3,13 +3,13 @@ public class Main
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-        System.out.println ("Ingresa una palabra: ");
-        String input = sc.next();
-        String[] palabras = input.split(" ");
-        System.out.println("La palabra invertida es: ");
-        for (String palabra : palabras) {
-            String reverso = new StringBuilder(palabra).reverse().toString();
-            System.out.print(reverso + " ");
+            System.out.println ("Ingresa una palabra: ");
+            String input = sc.next();
+            String[] palabras = input.split(" ");
+            System.out.println("La palabra invertida es: ");
+            for (String palabra : palabras) {
+		    String reverso = new StringBuilder(palabra).reverse().toString();
         }
+	System.out.print(reverso + " ");
     }
 }
